2775c78317065680a44439b6a25ea456fd0d73f4
Anatoliy Kadochnikov <an.kadochnikov@gmail.com>
2024-07-28 16:17:52 +0500

    дополнил init.sql

--- MySQL_docker/init.sql
+++ MySQL_docker/init.sql
@@ -3,15 +3,16 @@ USE medical_center;
 
 CREATE TABLE `specialist` (
   `id` int UNIQUE PRIMARY KEY NOT NULL,
-  `surname` varchar(255),
-  `first_name` varchar(255),
-  `patronymic` varchar(255),
-  `speciality` varchar(255)
+  `surname` varchar(50),
+  `first_name` varchar(50),
+  `patronymic` varchar(50),
+  `speciality` text
 );
 
 CREATE TABLE `service` (
   `id` int UNIQUE PRIMARY KEY NOT NULL,
-  `name` varchar(255)
+  `name` varchar(255),
+  `description` text
 );
 
 CREATE TABLE `price` (
@@ -30,18 +31,18 @@ CREATE TABLE `specialist_service` (
 
 CREATE TABLE `client` (
   `id` int UNIQUE PRIMARY KEY NOT NULL,
-  `surname` varchar(255),
-  `first_name` varchar(255),
-  `patronymic` varchar(255),
-  `phone` varchar(255)
+  `surname` varchar(50),
+  `first_name` varchar(50),
+  `patronymic` varchar(50),
+  `phone` varchar(18)
 );
 
 CREATE TABLE `visit` (
   `id` int UNIQUE PRIMARY KEY NOT NULL,
   `client_fk` int,
   `date` datetime,
-  `purpose` varchar(255),
-  `comment` varchar(255)
+  `purpose` text,
+  `comment` text
 );
 
 CREATE TABLE `service_provided` (
@@ -55,37 +56,43 @@ CREATE TABLE `examination_result` (
   `id` int UNIQUE PRIMARY KEY NOT NULL,
   `service_provided_fk` int,
   `date` datetime,
-  `result` varchar(255)
+  `comment` text,
+  `results` json
 );
 
 CREATE TABLE `visit_specialist` (
   `id` int UNIQUE PRIMARY KEY NOT NULL,
   `visit_fk` int,
   `specialist_fk` int,
-  `comment` varchar(255)
+  `comment` text,
+  `recommendations` mediumtext
 );
 
-CREATE INDEX `specialist_index_0` ON `specialist` (`surname`);
+CREATE INDEX `idx_specialist_surname` ON `specialist` (`surname`);
 
-CREATE INDEX `specialist_index_1` ON `specialist` (`speciality`);
+CREATE INDEX `idx_specialist_speciality` ON `specialist` (`speciality`);
 
-CREATE UNIQUE INDEX `service_index_2` ON `service` (`name`);
+CREATE UNIQUE INDEX `uniq_idx_service_name` ON `service` (`name`);
 
-CREATE UNIQUE INDEX `price_index_3` ON `price` (`service_fk`, `begin_date`, `end_date`);
+CREATE UNIQUE INDEX `uniq_idx_price_service_dates` ON `price` (`service_fk`, `begin_date`, `end_date`);
 
-CREATE UNIQUE INDEX `specialist_service_index_4` ON `specialist_service` (`service_fk`, `specialist_fk`);
+CREATE UNIQUE INDEX `uniq_idx_specialist_service_service_specialist` ON `specialist_service` (`service_fk`, `specialist_fk`);
 
-CREATE INDEX `client_index_5` ON `client` (`surname`) USING HASH;
+CREATE INDEX `idx_client_surname` ON `client` (`surname`) USING HASH;
 
-CREATE UNIQUE INDEX `client_index_6` ON `client` (`phone`);
+CREATE UNIQUE INDEX `uniq_idx_client_phone` ON `client` (`phone`);
 
-CREATE INDEX `visit_index_7` ON `visit` (`client_fk`);
+CREATE INDEX `idx_visit_client` ON `visit` (`client_fk`);
 
-CREATE INDEX `service_provided_index_8` ON `service_provided` (`visit_fk`, `service_fk`);
+CREATE INDEX `idx_service_provided_visit_service` ON `service_provided` (`visit_fk`, `service_fk`);
 
-CREATE INDEX `examination_result_index_9` ON `examination_result` (`service_provided_fk`);
+CREATE INDEX `idx_examination_result_service_provided` ON `examination_result` (`service_provided_fk`);
 
-CREATE INDEX `visit_specialist_index_10` ON `visit_specialist` (`specialist_fk`, `visit_fk`);
+CREATE INDEX `idx_visit_specialist_visit_specialist` ON `visit_specialist` (`specialist_fk`, `visit_fk`);
+
+CREATE INDEX idx_service_dates ON `price` (`begin_date`, `end_date`);
+
+CREATE FULLTEXT INDEX fulltext_idx_service_name_description ON `service` (`name`, `description`);
 
 ALTER TABLE `price` ADD FOREIGN KEY (`service_fk`) REFERENCES `service` (`id`);
 
@@ -105,6 +112,15 @@ ALTER TABLE `visit_specialist` ADD FOREIGN KEY (`visit_fk`) REFERENCES `visit` (
 
 ALTER TABLE `visit_specialist` ADD FOREIGN KEY (`specialist_fk`) REFERENCES `specialist` (`id`);
 
+ALTER TABLE visit PARTITION BY RANGE (TO_DAYS(date))
+(
+    PARTITION older VALUES LESS THAN (TO_DAYS('2024-07-01')),
+    PARTITION p202407 VALUES LESS THAN (TO_DAYS('2024-08-01')),
+    PARTITION p202408 VALUES LESS THAN (TO_DAYS('2024-09-01')),
+    PARTITION p202409 VALUES LESS THAN (TO_DAYS('2024-10-01')),
+    PARTITION p202410 VALUES LESS THAN (TO_DAYS('2024-11-01')),
+    PARTITION newer VALUES LESS THAN (TO_DAYS('9999-12-31'))
+);
 
 -- Заполнение тестовыми данными
 
@@ -114,9 +130,9 @@ ALTER TABLE `visit_specialist` ADD FOREIGN KEY (`specialist_fk`) REFERENCES `spe
 -- (3, 'Сидоров', 'Сидор', 'Сидорович', '333333333');
 
 -- insert into visit (id, client_fk, date, purpose, comment)
--- values (1, 1, '2024-04-20', 'Обследование', '111111111'),
--- (2, 2, '2024-05-20', 'Медкомиссия', '222222222'),
--- (3, 1, '2024-06-10', 'Повторный осмотр', '333333333');
+-- values (1, 1, '2024-07-20', 'Обследование', '111111111'),
+-- (2, 2, '2024-08-20', 'Медкомиссия', '222222222'),
+-- (3, 1, '2025-06-10', 'Повторный осмотр', '333333333');
 
 -- insert into specialist (id, surname, first_name, patronymic, speciality)
 -- values
